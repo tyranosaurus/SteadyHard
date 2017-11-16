013b49968a0a817f7995eebefa6f429ad5f6de60
tyrano <tyranosaurus@nate.com>
2017-11-16 20:00:01 +0900

    test butterknife

--- app/src/main/java/com/tyranotyrano/steadyhard/MainActivity.java
+++ app/src/main/java/com/tyranotyrano/steadyhard/MainActivity.java
@@ -2,14 +2,19 @@ package com.tyranotyrano.steadyhard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // create youngjin branch
-        // create test branch
+
+        textView.setText("버터나이프 테스트");
     }
 }
