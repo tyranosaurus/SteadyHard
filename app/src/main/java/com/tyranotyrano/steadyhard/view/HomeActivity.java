db35cacb92295c9b9cf45b4b8c30d4458d18cf2e
tyrano <tyranosaurus@nate.com>
2017-11-19 17:09:18 +0900

    modify activity_home.xml

--- app/src/main/java/com/tyranotyrano/steadyhard/view/HomeActivity.java
+++ app/src/main/java/com/tyranotyrano/steadyhard/view/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.tyranotyrano.steadyhard.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.tyranotyrano.steadyhard.R;
 
@@ -11,5 +17,25 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /** 툴바 세팅 */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // 앱 기본 이름 안보이게 하는 것.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_searchview, menu);
+
+        /** SearchView 설정 */
+        MenuItem searchItem = menu.findItem(R.id.menuItemSearch);
+        // 1. SearchView : hint 설정
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("검색어를 입력하세요.");
+        // 2. SearchView : underline 제거
+        searchView.findViewById(android.support.v7.appcompat.R.id.search_plate).setBackgroundColor(Color.parseColor("#ffffffff"));
+
+        return true;
     }
 }
